6ed168d5c9033480670e8ace8f55fe269c0249ab
jill64 <intents.turrets0h@icloud.com>
2023-11-02 21:59:25 +0900

    Add tests and implementation for parseAttributes and transform functions

---
+++ src/lib/utils/transform.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from 'vitest'
+import { transform } from './transform'
+
+test('transform', () => {
+  expect(transform('<html>', {})).toBe('<html>')
+
+  expect(transform('<html>', { lang: 'en' })).toBe('<html lang="en">')
+
+  expect(transform('<a>', { lang: 'en' })).toBe('<a>')
+
+  expect(transform('<html lang="test">', { lang: 'en' })).toBe(
+    '<html lang="en">'
+  )
+
+  expect(transform('<html>', { prefix: 'og: http://ogp.me/ns#' })).toBe(
+    '<html prefix="og: http://ogp.me/ns#">'
+  )
+
+  expect(
+    transform("<html lang='test'>", {
+      lang: 'en',
+      prefix: 'og: http://ogp.me/ns#'
+    })
+  ).toBe('<html lang="en" prefix="og: http://ogp.me/ns#">')
+
+  expect(
+    transform('<html>', { lang: 'en', prefix: 'og: http://ogp.me/ns#' })
+  ).toBe('<html lang="en" prefix="og: http://ogp.me/ns#">')
+})
