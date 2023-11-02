305836c3306497e42a1d6509e5bae0a184261615
jill64 <intents.turrets0h@icloud.com>
2023-11-02 23:51:02 +0900

    Remove deprecated bind function and export transform function instead.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
 export { default as SvelteHTML } from './SvelteHtml.svelte'
-// eslint-disable-next-line deprecation/deprecation
-export { bind } from './bind'
 export { transform } from './transform'
