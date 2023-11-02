234f8bab83913db126d753df4d605043aa77238f
jill64 <intents.turrets0h@icloud.com>
2023-11-02 23:41:57 +0900

    Deprecate bind in favor of transform

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as SvelteHTML } from './SvelteHtml.svelte'
+// eslint-disable-next-line deprecation/deprecation
 export { bind } from './bind'
 export { transform } from './transform'
