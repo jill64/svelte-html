06472dbb0d3f81cb4aecdf3030cea22bbbdd5d47
jill64 <143883742+jill64@users.noreply.github.com>
2023-11-10 07:21:46 +0000

    Refactor transform function to apply attributes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
-import { transform } from '$lib'
+import { apply } from '$lib'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 
 const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
 
 export const handle = onHandle(({ resolve, event }) =>
   resolve(event, {
-    transformPageChunk: transform({
+    transformPageChunk: apply({
       lang: 'zh',
       prefix: 'https://example.com'
     })
