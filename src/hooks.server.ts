af891f92c3236860caa5e4942e1089053e116547
jill64 <intents.turrets0h@icloud.com>
2023-11-11 12:56:15 +0900

    Add dark theme and sequence hook to server hooks.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,20 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { apply } from '$lib'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { onRender } from '@jill64/svelte-dark-theme'
+import { sequence } from '@sveltejs/kit/hooks'
 
 const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
 
-export const handle = onHandle(({ resolve, event }) =>
-  resolve(event, {
-    transformPageChunk: apply({
-      lang: 'en',
-      prefix: 'https://example.com'
+export const handle = onHandle(
+  sequence(onRender(), ({ resolve, event }) =>
+    resolve(event, {
+      transformPageChunk: apply({
+        lang: 'en',
+        prefix: 'https://example.com'
+      })
     })
-  })
+  )
 )
 
 export const handleError = onError()
