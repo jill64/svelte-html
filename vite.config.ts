0efed97e23b75b7f10787d0d921a6fbb08bbc9d2
jill64 <contact@jill64.dev>
2025-01-21 17:48:39 +0900

    chore: remove TypeScript workaround comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  // @ts-expect-error workaround for vite issue
   plugins: [sveltekit()],
   test: {
     include: ['**/*.test.ts']
